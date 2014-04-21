48e47a0545763ef657b5ef1329f55cb77c96f7fb
Matt McLaughlin <mclaughlin.matt@gmail.com>
2014-04-20 19:53:19 -0700

    Fixes #2

--- example/app.js
+++ example/app.js
@@ -10,6 +10,11 @@ var users = require('./routes/users');
 
 var app = express();
 
+var Aerie = require('../');
+Aerie.locals({
+  name: 'Tony Stark'
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
