5e876685c7adf43c1d4dfdbdf4518133f0b379c6
Matt McLaughlin <mclaughlin.matt@gmail.com>
2014-04-28 15:34:26 -0700

    started to stub out the view object

--- example/routes/index.js
+++ example/routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var HomeView = require('../views/home')
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  var view = new HomeView();
+  res.render(view, { title: 'Express' });
 });
 
 module.exports = router;
---
+++ example/views/home.js
@@ -0,0 +1,5 @@
+'use strict';
+
+var View = require('../../').View;
+
+var HomeView = View.extend('home', 1, 'index');
